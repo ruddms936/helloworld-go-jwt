d856a2e865d8518fdcf3739d47e1e5440ce37afc
Kyungeun <ruddms936@naver.com>
2021-02-11 17:04:20 +0900

    add: Create Token

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,6 +25,22 @@ var user = User{
 var (
 	router = gin.Default()
 )
+
+func CreateToken(userid uint64) (string, error) {
+	var err error
+	// Access Token 생성
+	os.Setenv("ACCESS_SECRET", "jdnfksdmfksd")
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userid
+	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
 
 func Login(c *gin.Context) {
 	var u User
