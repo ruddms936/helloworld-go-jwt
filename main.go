2998ff87b89f3da56826ecaf0bf5a4cc3672c7ad
Kyungeun <ruddms936@naver.com>
2021-02-11 11:24:10 +0900

    add

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,6 +22,25 @@ var user = User{
 var (
 	router = gin.Default()
 )
+
+func Login(c *gin.Context) {
+	var u User
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
+		return
+	}
+	// 사용자 요청 값과 비교합니다.
+	if user.Username != u.Username || user.Password != u.Password {
+		c.JSON(http.StatusUnauthorized, "Please provide valid login details")
+		return
+	}
+	token, err := CreateToken(user.ID)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, token)
+}
 
 func main() {
 	router.POST("/login", Login)
